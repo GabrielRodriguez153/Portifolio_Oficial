4bcdb3675e72562e8596d2868e47318e72824020
Gabriel Henrique <gabrodriguez153@gmail.com>
2025-11-25 09:07:20 -0300

    feat: add README.md in project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,7 +4,8 @@ export const projectsData = [
   {
     id: 1,
     title: "API para Projeto Integrador Mobile",
-    description: "Uma API RESTful moderna para o sistema AntraVision, que monitora a saúde de plantações usando dados de sensores (umidade) e análises de antracnose em mudas de pupunheiras.",
+    description:
+      "Uma API RESTful moderna para o sistema AntraVision, que monitora a saúde de plantações usando dados de sensores (umidade) e análises de antracnose em mudas de pupunheiras.",
     technologies: ["Node.js", "MongoDB", "Express", "AWS EC2"],
     gradient: "from-blue-500 to-purple-600",
     icon: FaGlobe,
@@ -14,21 +15,32 @@ export const projectsData = [
   {
     id: 2,
     title: "Projeto Integrador - Versão Final",
-    description: "Uma plataforma inteligente para monitoramento, análise e prevenção de antracnose em plantações de pupunheira.",
-    technologies: ["ReactJS", "TailwindCSS", "Vite", "Flask", "MongoDB", "AWS EC2"],
+    description:
+      "Uma plataforma inteligente para monitoramento, análise e prevenção de antracnose em plantações de pupunheira.",
+    technologies: [
+      "ReactJS",
+      "TailwindCSS",
+      "Vite",
+      "Flask",
+      "MongoDB",
+      "AWS EC2",
+    ],
     gradient: "from-green-500 to-teal-600",
     icon: FaDesktop,
     category: "FullStack",
-    githubUrl: "https://github.com/GabrielRodriguez153/Projeto_Integrador-Versao_Final.git",
+    githubUrl:
+      "https://github.com/GabrielRodriguez153/Projeto_Integrador-Versao_Final.git",
   },
   {
     id: 3,
     title: "Vitrine - Laboratorio de Práticas",
-    description: "A Vitrine de Projetos é uma interface pública desenvolvida em React/Next.js que permite a visualização e votação em representantes de classes",
-    technologies: ["ReactJS", "Next.JS", "NodeJS"],
+    description:
+      "A Vitrine de Projetos é uma interface pública desenvolvida em React/Next.js que permite a visualização e votação em representantes de classes",
+    technologies: ["React.JS", "Next.JS", "Node.JS"],
     gradient: "from-orange-500 to-red-600",
     icon: FaRegNewspaper,
     category: "Frontend",
-    githubUrl: "https://github.com/laboratorio-de-praticas-2025-1/vitrine-fe.git",
+    githubUrl:
+      "https://github.com/laboratorio-de-praticas-2025-1/vitrine-fe.git",
   },
 ];
